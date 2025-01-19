2130eed6f11fa4bdb55340773bca83336f7b06d8
allaine-sioson <allaine.sioson@gmail.com>
2025-01-19 07:48:13 -0500

    test: complete unit tests for Skincare class methods

--- src/application/unitTests/SkincareTest.java
+++ src/application/unitTests/SkincareTest.java
@@ -1,6 +1,9 @@
 package application.unitTests;
 
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
+import java.util.*;
+import application.medicines.*;
+import application.*;
 
 /**
  * @author: Allaine
@@ -9,53 +12,116 @@ import org.junit.jupiter.api.Test;
  */
 
 public class SkincareTest {
-    @Test
-    void testAddConcern() {
 
+    @Test
+    void testGetConcerns1() {
+        // Arrange
+        Skincare skincare = new Skincare();
+        skincare.addConcern("Dryness");
+        skincare.addConcern("Acne");
+
+        // Act
+        ArrayList<String> actual = skincare.getConcerns();
+        
+        // Assert
+        ArrayList<String> expected = new ArrayList<>();
+        expected.add("Dryness");
+        expected.add("Acne");
+        Assertions.assertEquals(expected, actual);
     }
 
     @Test
-    void testAddMedication() {
+    void testGetConcerns2() {
+        // Arrange
+        Skincare skincare = new Skincare();
+        skincare.addConcern("Wrinkles");
+        skincare.addConcern("Dark Spots");
+        skincare.addConcern("Whiteheads");
 
+        // Act
+        ArrayList<String> actual = skincare.getConcerns();
+        
+        // Assert
+        ArrayList<String> expected = new ArrayList<>();
+        expected.add("Wrinkles");
+        expected.add("Dark Spots");
+        expected.add("Whiteheads");
+        Assertions.assertEquals(expected, actual);
     }
 
     @Test
-    void testAddStepToRoutine() {
+    void testGetMedication1() {
+        // Arrange
+        Skincare skincare = new Skincare();
+        skincare.addMedication(new Serum("Niacinamide", 2));
+        skincare.addMedication(new Cream("Hydrocortisone", 1));
 
+        // Act
+        ArrayList<Medicine> actual = skincare.getMedication();
+        
+        // Assert
+        ArrayList<Medicine> expected = new ArrayList<>();
+        expected.add(new Serum("Niacinamide", 2));
+        expected.add(new Cream("Hydrocortisone", 1));
+        Assertions.assertEquals(expected.toString(), actual.toString());
     }
 
     @Test
-    void testGetConcerns() {
+    void testGetMedication2() {
+        // Arrange
+        Skincare skincare = new Skincare();
+        skincare.addMedication(new Serum("Salicylic Acid", 2));
 
+        // Act
+        ArrayList<Medicine> actual = skincare.getMedication();
+        
+        // Assert
+        ArrayList<Medicine> expected = new ArrayList<>();
+        expected.add(new Serum("Salicylic Acid", 2));
+        Assertions.assertEquals(expected.toString(), actual.toString());
     }
 
     @Test
-    void testGetJSONSkincare() {
+    void testGetRoutine1() {
+        // Arrange
+        Skincare skincare = new Skincare();
+        skincare.addStepToRoutine("Wash Face");
+        skincare.addStepToRoutine("Apply Toner");
+        skincare.addStepToRoutine("Apply Serums");
+        skincare.addStepToRoutine("Moisturize");
 
+        // Act
+        ArrayList<String> actual = skincare.getRoutine();
+        
+        // Assert
+        ArrayList<String> expected = new ArrayList<>();
+        expected.add("1. Wash Face");
+        expected.add("2. Apply Toner");
+        expected.add("3. Apply Serums");
+        expected.add("4. Moisturize");
+        Assertions.assertEquals(expected, actual);
     }
 
     @Test
-    void testGetMedication() {
+    void testGetRoutine2() {
+        // Arrange
+        Skincare skincare = new Skincare();
+        skincare.addStepToRoutine("Wash Face");
+        skincare.addStepToRoutine("Apply Bioderm");
+        skincare.addStepToRoutine("Apply Lip mask");
+        skincare.addStepToRoutine("Moisturize");
+        skincare.addStepToRoutine("Apply Sunscreen");
 
-    }
-
-    @Test
-    void testGetRoutine() {
-
-    }
-
-    @Test
-    void testResetConcerns() {
-
-    }
-
-    @Test
-    void testResetMedication() {
-
-    }
-
-    @Test
-    void testResetRoutine() {
-
+        // Act
+        ArrayList<String> actual = skincare.getRoutine();
+        
+        // Assert
+        ArrayList<String> expected = new ArrayList<>();
+        expected.add("1. Wash Face");
+        expected.add("2. Apply Bioderm");
+        expected.add("3. Apply Lip mask");
+        expected.add("4. Moisturize");
+        expected.add("5. Apply Sunscreen");
+        Assertions.assertEquals(expected, actual);
     }
 }
