06dc2d43f370d1b9cde9493e082b860409fbf9d7
allaine-sioson <allaine.sioson@gmail.com>
2025-01-19 10:21:41 -0500

    test: complete unit tests for MealPlan class

--- src/application/unitTests/MealPlanTest.java
+++ src/application/unitTests/MealPlanTest.java
@@ -1,6 +1,7 @@
 package application.unitTests;
 
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
+import application.*;
 
 /**
  * @author: Allaine
@@ -10,27 +11,35 @@ import org.junit.jupiter.api.Test;
 
 public class MealPlanTest {
     @Test
-    void testResetMealPlan() {
+    void testToString1() {
+       // Arrange
+       MealPlan mealPlan = new MealPlan();
+       mealPlan.setBreakfast("pancakes");
+       mealPlan.setLunch("pesto pizza");
+       mealPlan.setDinner("mushroom pasta");
 
+       // Act
+       String actual = mealPlan.toString();
+
+       // Assert
+       String expected = "[\"breakfast: pancakes\", \"lunch: pesto pizza\", \"dinner: mushroom pasta\"]";
+       Assertions.assertEquals(expected, actual);
     }
 
     @Test
-    void testSetBreakfast() {
+    void testToString2() {
+       // Arrange
+       MealPlan mealPlan = new MealPlan();
+       mealPlan.setBreakfast("grilled cheese sandwich");
+       mealPlan.setLunch("salad");
+       mealPlan.setDinner("dumplings");
+       mealPlan.setDinner("ramen");
 
-    }
+       // Act
+       String actual = mealPlan.toString();
 
-    @Test
-    void testSetDinner() {
-
-    }
-
-    @Test
-    void testSetLunch() {
-
-    }
-
-    @Test
-    void testToString() {
-
+       // Assert
+       String expected = "[\"breakfast: grilled cheese sandwich\", \"lunch: salad\", \"dinner: ramen\"]";
+       Assertions.assertEquals(expected, actual);
     }
 }
